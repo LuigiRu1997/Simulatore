b90cbeef159ba2da87cd59ca500229eda551fa17
Luigi <s266152@studenti.polito.it>
2020-03-27 23:39:00 +0100

    Aggiunto input/output utente

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{ /*Questo è un commento */
+  int age;
+  printf ("enter your age \n");
+  scanf("%d", &age );
+  printf("Age value is %d"\n", &Age );
+
+}
