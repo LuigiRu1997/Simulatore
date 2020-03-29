4b4047d7f7eb64e613548cabff4453c00158595d
Luigi <luigi@iMac-di-Luigi.homenet.telecomitalia.it>
2020-03-29 21:47:21 +0200

    uadhdfuw

--- main.c
+++ main.c
@@ -3,10 +3,5 @@
 
 int main()
 {
-  int numeri[] = {10,20,30,40};
-  int index;
-  for (index=0; index<4; index ++) {
-    printf("%d ", numeri[index]);
-dddfgfg
-  }
+    printf("Hello World");    
 }
