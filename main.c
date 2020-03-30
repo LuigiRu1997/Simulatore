4108953c2cf3732253a98a419baeab007f917fc8
Luigi <s266152@studenti.polito.it>
2020-03-30 12:48:02 +0200

    rimosso ciao

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
 {
   FILE *pEngine = fopen("engine.txt", "r"); //definito puntatore & apertura FILE
   if (pEngine == 0) // controllo che il file sia stato aperto bene
-  //ciao
+  
     {
       perror("Errore in apertura del file");
       exit(1);
