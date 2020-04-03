df3c2594e54c40fac6e3416c397be106e82a8e01
Luigi <s266152@studenti.polito.it>
2020-04-03 20:09:03 +0200

    Prova commit

--- nicola/interpol1.c
+++ nicola/interpol1.c
@@ -9,33 +9,37 @@ int s=0;
 float x,da,m,cf;
 float funz(float,float,float,float,float);
 
-float funz(float x,float a1,float a2,float c1,float c2){
+float funz(float x,float a1,float a2,float c1,float c2)
+{
     da=x-a1;
     m=(c2-c1)/(a2-a1);
     cf=c1+(m*da);
     return cf;
 }
-int main(){
 
+
+int main()
+{
     printf("inserisci valore alpha\n");
     scanf("%f",&x);
     printf("\n");
-    while(x<a[0] || x>a[3]){
-        printf("[!]WARNING immettere valore alpha compreso nel range\n");
+    while(x<a[0] || x>a[3])
+    {
+        printf("[!]WARNING immettere valore alpha compreso nel range. Reinserire:\n");
         scanf("%f",&x);
     }
-    while(i){
-        if(x>=a[s] && x<=a[s+1]){
-        a1=a[s];
-        a2=a[s+1];
-        c1=c[s];
-        c2=c[s+1];
-        i=0; }
-        else{
-            s++;
-        }
-    }
-    cf=funz(x,a1,a2,c1,c2);
-    printf("il valore di cf è: %f\n",cf);
 
+    while(i){
+        if(x>=a[s] && x<=a[s+1])
+        {
+          a1=a[s];
+          a2=a[s+1];
+          c1=c[s];
+          c2=c[s+1];
+          i=0;
+        }
+        else{ s++; }
+        }
+    cf=funz(x,a1,a2,c1,c2);
+    printf("il valore di cf e': %f\n",cf);
 }
